e414b6f19988891cd2e316fb43eadd6a043e1d79
Poripouria <poripouria9@gmail.com>
2023-10-18 14:47:23 +0330

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+Description:
+    Bachelor's Project (Undergraduate Thesis)
+    "Hyperparameters Tuning of Deep Unfolding Networks for Image Enhancement"
+    Project for Amirkabir University of Technilogy (Tehran Polytechnic)
+    Computer Scince department
+    Supervisor: Dr. Fatemeh Shakeri
+
+Student Name & ID: Pouria Alimoradpor 9912035
+"""
+
