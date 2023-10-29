7c5ba765ae83398409e86b27dae01335294823b9
Poripouria <poripouria9@gmail.com>
2023-10-29 22:04:55 +0330

    Some Updates

--- Hybrid MPSO CNN/Hybrid-MPSO-CNN.py
+++ Hybrid MPSO CNN/Hybrid-MPSO-CNN.py
@@ -11,17 +11,17 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
 
 search_space = { """ Range of hyperparameters (Based on Table 1) """
-    'nC':   (1, 5),     # Number of convolutional layers
-    'nP':   (1, 5),     # Number of pooling layers
-    'nF':   (1, 5),     # Number of fully connected layers
-    'c_nf': (1, 64),    # Number of filters
-    'c_fs': (1, 13),    # Filter Size (odd) 
-    'c_pp': (0, 1),     # Padding pixels (0: "valid", 1: "same")
-    'c_ss': (1, 5),     # Stride Size (< 𝑐_fs)
-    'p_fs': (1, 13),    # Filter Size (odd)
-    'p_ss': (1, 5),     # Stride Size
-    'p_pp': (0, 1),     # Padding pixels (< 𝑝_fs) (0: "valid", 1: "same")
-    'op':   (1, 1024)   # Number of neurons
+    'nC':   (1, 5),         # Number of convolutional layers
+    'nP':   (1, 5),         # Number of pooling layers
+    'nF':   (1, 5),         # Number of fully connected layers
+    'c_nf': (1, 64),        # Number of filters
+    'c_fs': (1, 13),        # Filter Size (odd) 
+    'c_pp': (0, 1),         # Padding pixels (0: "valid", 1: "same")
+    'c_ss': (1, 5),         # Stride Size (< 𝑐_fs)
+    'p_fs': (1, 13),        # Filter Size (odd)
+    'p_ss': (1, 5),         # Stride Size
+    'p_pp': (0, 1),         # Padding pixels (< 𝑝_fs) (0: "valid", 1: "same")
+    'op':   (1, 1024)       # Number of neurons
 }
 
 def calculate_omega(t, t_max, alpha=0.2):
@@ -64,18 +64,40 @@ class Particle_Swarm_L2:
 class CNN:
     def __init__(self, hyperparameters):
         self.hyperparameters = hyperparameters
-        self.nC = nC        # number of convolution layers
-        self.nP = nP        # number of pooling layers
-        self.nF = nF        # number of fully connected layers
-        self.c_nf = c_nf    # number of filters in the convolutional layer
-        self.c_fs = c_fs    # size of filter/kernel in the convolutional layer
-        self.c_pp = c_pp    # padding (valid or same) requirement in the convolutional layer
-        self.c_ss = c_ss    # size of stride in the convolutional layer
-        self.p_fs = p_fs    # size of a filter in the max pooling layer
-        self.p_ss = p_ss    # size of stride in the max-pooling layer
-        self.p_pp = p_pp    # padding pixels in pooling layer
-        self.op = op        # number of output neurons in the fully connected layer
+        self.nC = hyperparameters['nC']        # number of convolution layers
+        self.nP = hyperparameters['nP']        # number of pooling layers
+        self.nF = hyperparameters['nF']        # number of fully connected layers
+        self.c_nf = hyperparameters['c_nf']    # number of filters in the convolutional layer
+        self.c_fs = hyperparameters['c_fs']    # size of filter/kernel in the convolutional layer
+        self.c_pp = hyperparameters['c_pp']    # padding (valid or same) requirement in the convolutional layer
+        self.c_ss = hyperparameters['c_ss']    # size of stride in the convolutional layer
+        self.p_fs = hyperparameters['p_fs']    # size of a filter in the max pooling layer
+        self.p_ss = hyperparameters['p_ss']    # size of stride in the max-pooling layer
+        self.p_pp = hyperparameters['p_pp']    # padding pixels in pooling layer
+        self.op = hyperparameters['op']        # number of output neurons in the fully connected layer
 
     def buid_model(self):
         model = Sequential()
+
+        # Add convolution and pooling layers according to the hyperparameters
+        for i in range(self.nC):
+            model.add(layers.Conv2D(self.c_nf[i], 
+                                    self.c_fs[i], 
+                                    strides = self.c_ss[i], 
+                                    padding = self.c_pp[i], 
+                                    activation = 'relu'))
+            if i < self.nP:
+                model.add(layers.MaxPooling2D(self.p_fs[i], 
+                                              strides = self.p_ss[i], 
+                                              padding = self.p_pp[i]))
+        # Flatten the output before adding fully connected layers
+        model.add(layers.Flatten())
+        # Add fully connected layers according to the hyperparameters
+        for j in range(self.nF):
+            model.add(layers.Dense(self.op[j], 
+                      activation = 'relu'))
+        # Add the final output layer with 10 neurons for 10 classes and softmax activation
+        model.add(layers.Dense(10, activation = 'softmax'))
+
+        return model
 
