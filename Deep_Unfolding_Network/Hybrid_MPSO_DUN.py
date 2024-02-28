d87891ef61aa40cbcd02753dc62b7c0cd0e83b88
Poripouria <poripouria9@gmail.com>
2024-02-28 13:32:56 +0330

    Add code to save and load the model.

--- Deep_Unfolding_Network/Hybrid_MPSO_DUN.py
+++ Deep_Unfolding_Network/Hybrid_MPSO_DUN.py
@@ -3,4 +3,9 @@ Description:
     Implementation of "Hybrid MPSO-DUN: Multi-level Particle Swarm optimized hyperparameters of Deep Unfolding Network"
     doi: https://doi.org/10.48550/arXiv.1812.04276
 """
+import random
+import math
+import torch
+import torch.nn as nn
+from iRestNet.Model_files import iRestNet
 
